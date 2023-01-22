ceec03d55b0745f03e6c056d85f3da5aaf6faaab
heosujinnn <0204sujin@naver.com>
2023-01-23 04:34:49 +0900

    day1

---
+++ app/src/main/java/com/soojin/storysns/SignUPActivity.java
@@ -0,0 +1,89 @@
+package com.soojin.storysns;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignUPActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private static final String TAG="signUpActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
+        @Override
+        public void onStart() {
+            super.onStart();
+            // Check if user is signed in (non-null) and update UI accordingly.
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+//            if (currentUser != null) {
+//                reload();
+//            }
+
+        findViewById(R.id.signUp_btn).setOnClickListener(onClickListener);
+
+}
+View.OnClickListener onClickListener=new View.OnClickListener(){
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.signUp_btn:
+                Log.e("클릭","클릭");
+                signUp();
+                break;
+        }
+    }
+};
+    private void signUp() {
+        String email = ((EditText) findViewById(R.id.email_et)).getText().toString();
+        String password = ((EditText) findViewById(R.id.password_et)).getText().toString();
+        String passwordCheck = ((EditText) findViewById(R.id.password_check_et)).getText().toString();
+        if (email.length() > 0 && password.length() > 0 && passwordCheck.length() > 0) {
+
+            if (password.equals(passwordCheck)) {
+
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(SignUPActivity.this, "회원가입에 성공하였습니다.", Toast.LENGTH_SHORT).show();
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    //성공 ui
+                                } else {
+                                    if (task.getException() != null) {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w(TAG, "fail", task.getException());
+                                        Toast.makeText(SignUPActivity.this, "회원가입에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+                                        //실패->ui
+                                    }
+                                }
+                            }
+                        });
+            } else {
+                Toast.makeText(SignUPActivity.this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            Toast.makeText(this, "이메일 또는 비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show();
+        }
+    }
+    
+}
